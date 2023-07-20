e6aeaea84a4eef810bba4d1abb91b19dd70e0eed
Mattia Risiglione <mattia.risiglione@iit.it>
2023-07-20 11:40:00 +0100

    removed ctrl_limits check

--- rai/Kin/frame.cpp
+++ rai/Kin/frame.cpp
@@ -1123,9 +1123,10 @@ void rai::Joint::read(const Graph& G) {
   G.get(ctrl_limits, "ctrl_limits");
   if(ctrl_limits.N && type!=JT_rigid) {
     if(!limits.N) limits.resizeAs(ctrl_limits).setZero();
-    CHECK_EQ(3, ctrl_limits.N, "parsed ctrl_limits have wrong dimension");
+    //CHECK_EQ(1, ctrl_limits.N, "parsed ctrl_limits have wrong dimension");
     limits.append(ctrl_limits);
   }
+
 
   //coupled to another joint requires post-processing by the Graph::read!!
   if(G["mimic"]) {
